ff62154ac8c44b59b93c2f9fecf6459d406a5c6f
Cravat5386 <56939620+Cravat5386@users.noreply.github.com>
2021-11-25 18:02:28 -0500

    All the code needed to run stuff

--- PKPD/define_variables.m
+++ PKPD/define_variables.m
@@ -1,4 +1,4 @@
-function [k21, k31, p1, p2, p3] = define_variables(age, k10)
+function [x, y, a, b, c] = define_variables(age, k10)
     V1 = 4.27;
     V2 = 18.9-0.391*(age - 53);
     V3 = 2.38;
@@ -8,8 +8,9 @@ function [k21, k31, p1, p2, p3] = define_variables(age, k10)
     k13 = C13/V1;
     k21 = C12/V2;
     k31 = C13/V3;
-    a = k10*k21*k31;
+    c = k10*k21*k31;
     b = k10*k21*k31 + k31*k12*k21 + k13*k21;
-    c = k10 + k12 + k13 + k21 + k31;
-    [p1, p2, p3] = roots([1 a b c]);
+    a = k10 + k12 + k13 + k21 + k31;
+    x = k21 + k31;
+    y = k21*k31;
 end
